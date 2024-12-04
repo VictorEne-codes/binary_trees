34e5b255a542b15241ac0e5c71352109f103e67d
VictorEne-Codes <omalevicene@gmail.com>
2024-12-04 13:24:05 -0800

    checking

--- binary_trees.h
+++ binary_trees.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <stdargs.h>
+#include <stdarg.h>
 #include <stdlib.h>
 
 /**
@@ -32,5 +32,7 @@ typedef struct binary_tree_s heap_t;
 
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
+
 
 #endif
